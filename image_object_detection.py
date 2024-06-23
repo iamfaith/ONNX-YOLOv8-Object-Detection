d2f5f835b7b395c586144b9c88731d1d2605e6e7
faith <faith@com>
2024-06-23 19:43:34 +0800

    update

--- image_object_detection.py
+++ image_object_detection.py
@@ -4,7 +4,8 @@ from imread_from_url import imread_from_url
 from yolov8 import YOLOv8
 
 # Initialize yolov8 object detector
-model_path = "models/yolov8m.onnx"
+model_path = "models/yolov8n-sim.onnx"
+# model_path = '/home/faith/ONNX-YOLOv8-Object-Detection/yolov8n.onnx'
 yolov8_detector = YOLOv8(model_path, conf_thres=0.2, iou_thres=0.3)
 
 # Read image
@@ -14,9 +15,10 @@ img = imread_from_url(img_url)
 # Detect Objects
 boxes, scores, class_ids = yolov8_detector(img)
 
+print(boxes, scores, class_ids)
 # Draw detections
 combined_img = yolov8_detector.draw_detections(img)
-cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
-cv2.imshow("Detected Objects", combined_img)
+# cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
+# cv2.imshow("Detected Objects", combined_img)
 cv2.imwrite("doc/img/detected_objects.jpg", combined_img)
-cv2.waitKey(0)
+# cv2.waitKey(0)
